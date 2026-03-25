a278fc528552c54005843990ce1cdae1baa0c97e
Dhruv-Tondon <tdhruv350@gmail.com>
2026-03-25 22:26:46 +0530

    reset logic added
    added a reset logic so that when reseted rx_sync2 doesnt retain its value. bug fixed.

--- rx_synchroniser.v
+++ rx_synchroniser.v
@@ -1,5 +1,6 @@
 module rx_sync (
     input  logic clk,
+    input  logic rst,
     input  logic rx_line,
     output logic rx_sync2
 );
@@ -7,9 +8,13 @@ module rx_sync (
 logic rx_sync1;
 
 always_ff @(posedge clk or posedge rst) begin
-    rx_sync1 <= rx_line;
-    rx_sync2 <= rx_sync1;
+    if (rst) begin
+        rx_sync1 <= 1;
+        rx_sync2 <= 1;
+    end else begin
+        rx_sync1 <= rx_line;
+        rx_sync2 <= rx_sync1;
+    end
 end
-
 
 endmodule
