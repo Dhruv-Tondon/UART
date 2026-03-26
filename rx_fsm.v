b744faf8f7082757e99fe15983897fbcb1d97821
Dhruv-Tondon <tdhruv350@gmail.com>
2026-03-26 23:44:05 +0530

    Update rx_fsm.v
    shift register implemented in a new module ,

--- rx_fsm.v
+++ rx_fsm.v
@@ -1,81 +1,51 @@
 module rx_ctrl (
-    input  logic clk, rst,rx_sync2,start,start_sampled,
-    input  logic data_sample,stop,stop_sampled,
-    input  logic rx_en,
-
-    output logic error,successful_frame,busy,
-    output logic [7:0] data_received
+    input  logic clk,rst, rx_sync2,start,
+    input  logic stop,start_sampled,data_sample,stop_sampled,
+    output logic shift_en,error,successful_frame,busy
 );
 
 typedef enum logic [2:0] {IDLE, START, DATA, STOP, DONE} state_t;
 state_t state, next;
 
 logic [2:0] bit_count;
-logic [7:0] shift_reg;
 
 always_ff @(posedge clk or posedge rst)
     if (rst) state <= IDLE;
     else state <= next;
 
 always_ff @(posedge clk or posedge rst) begin
-    if (rst)
-        bit_count <= 0;
-    else if (state != DATA)
-        bit_count <= 0;
-    else if (data_sample)
-        bit_count <= bit_count + 1;
+    if (rst) bit_count <= 0;
+    else if (state != DATA) bit_count <= 0;
+    else if (data_sample) bit_count <= bit_count + 1;
 end
-
-always_ff @(posedge clk or posedge rst) begin
-    if (rst)
-        shift_reg <= 0;
-    else if (state == DATA && data_sample)
-        shift_reg <= {rx_sync2, shift_reg[7:1]};  /
-end
-
 
 always_comb begin
     next = state;
     case(state)
-        IDLE:  if (start) next = START;
-
-        START: begin
-            if (start_sampled) next = DATA;
-            else    next = IDLE;
-        end
-
-        DATA: begin
-            if (data_sample && bit_count == 7)
-                next = STOP;
-        end
-
-        STOP: begin
-            if (stop_sampled) next = DONE;
-            else  next = IDLE;
-        end
-
+        IDLE: if (start) next = START;
+        START: if (start_sampled) next = DATA;
+               else next = IDLE;
+        DATA: if (data_sample && bit_count == 7) next = STOP;
+        STOP:  if (stop_sampled) next = DONE;
+               else next = IDLE;
         DONE: next = IDLE;
     endcase
 end
 
 always_comb begin
+    shift_en = 0;
     error = 0;
     successful_frame = 0;
     busy = 1;
-    data_received = shift_reg;
 
     case(state)
-        IDLE: begin
-            busy = 0;
-        end
+        IDLE: busy = 0;
 
-        START: 
-            if (!start_sampled)error = 1;
+        START: if (!start_sampled) error = 1;
 
-        DATA: 
+        DATA: if (data_sample) shift_en = 1;
 
-        STOP: 
-            if (!stop_sampled)error = 1;
+        STOP: if (!stop_sampled) error = 1;
 
         DONE: begin
             successful_frame = 1;
