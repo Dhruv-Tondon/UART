3104902cd8b58750466404267e5c4ed3f67dac52
Dhruv-Tondon <tdhruv350@gmail.com>
2026-03-27 13:26:53 +0530

    start detector for my rx
    this module is able to detect start signal ie when rx_line turns 0 from 1 .

---
+++ start_detector.v
@@ -0,0 +1,20 @@
+module start_detector (
+    input  rst, clk, rx_sync2,
+    output logic start
+);
+
+logic prev;
+always_ff @(posedge clk or posedge rst) begin
+    if (rst) begin
+        prev  <= 1;
+        start <= 0;
+    end
+    else begin
+        start <=(prev == 1 && rx_sync2 == 0);
+        prev<= rx_sync2;
+    end
+end
+
+endmodule
+`timescale 1ns/1ps
+
